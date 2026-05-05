f2b928bd828f8be95821373755909184e80218d9
Rahul Bobadi <66925050+Rahulbobadi@users.noreply.github.com>
2026-05-05 18:51:57 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Working 🚀');
+  res.send('App is Working Successfully');
 });
 
 app.listen(3000, () => {
